8bea3b1ba31baad686777929de51e7e642370c6f
i-ght <toph.ght@gmail.com>
2022-08-07 17:18:18 -0600

    lib

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 for dotc_file in ./src/*.c; do
-  clang -g -c "$dotc_file" -I./include/ -I./src/ -o "$dotc_file.o"
+  clang -g -fPIC -c "$dotc_file" -I./include/ -I./src/ -o "$dotc_file.o"
 done
 
 doto_files=""
@@ -8,5 +8,8 @@ for doto_file in ./src/*.c.o; do
   doto_files="${doto_files} ${doto_file}"
 done
 
-clang -o bin/program $doto_files
+ar rcs bin/libconduit.a $doto_files
+clang -g -shared -o bin/libconduit.so $doto_files
+ 
+clang -g -o bin/program -L./bin/ -lconduit
 rm ./src/*.c.o
